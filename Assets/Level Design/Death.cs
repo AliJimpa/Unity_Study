e3476ed15aadc4238b1bc4ddf1f19609c9497916
Alijimpa <alibadpa007@gmail.com>
2025-11-27 21:10:53 +0330

    Import My Level Design class asset & my practice

---
+++ Assets/Level Design/Death.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public int doorID;
+    private void OnDestroy() {
+        Operator.instance.Iamdone(doorID);
+        if (doorID == 3)
+        {
+            Operator.instance.Imindor3();
+        }
+    }
+}
---
+++ Assets/Level Design/Trigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Faz2;
+    private void OnTriggerEnter(Collider other) {
+        if (Operator.instance.EV01 == 15)
+        {
+            Faz2.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
